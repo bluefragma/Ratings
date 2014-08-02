64f6f2190be65d9644ad20a77f1a119d5ab3ea86
bluefragma <dkyang73@gmail.com>
2014-08-02 11:04:40 +0900

    PlayerCell 클래스 추가

---
+++ Ratins/PlayerCell.h
@@ -0,0 +1,13 @@
+//
+//  PlayerCell.h
+//  Ratins
+//
+//  Created by 양동길 on 2014. 8. 2..
+//  Copyright (c) 2014년 양동길. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerCell : UITableViewCell
+
+@end
