e421cc22e161690754378b19031cf1ca9b9e898f
bluefragma <dkyang73@gmail.com>
2014-08-02 09:43:47 +0900

    PlayersVewController 추가

---
+++ Ratins/PlayersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayersTableViewController.h
+//  Ratins
+//
+//  Created by 양동길 on 2014. 8. 2..
+//  Copyright (c) 2014년 양동길. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersTableViewController : UITableViewController
+
+@end
